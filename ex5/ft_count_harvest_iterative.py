a60abb60df9f0100ef0f951d5cd6c71a7c9e57f5
Ferdaous El Idrissi <feel-idr@e2r13p5.1337.ma>
2026-02-05 21:42:22 +0100

    done

---
+++ ex5/ft_count_harvest_iterative.py
@@ -0,0 +1,6 @@
+def ft_count_harvest_iterative():
+    day = int(input("Days until harvest: "))
+    bigin = 1
+    for bigin in range(day):
+        print("Day ", bigin + 1)
+    print("Harvest time!")
---
+++ ex5/ft_count_harvest_recursive.py
@@ -0,0 +1,10 @@
+def ft_count_harvest_recursive():
+    days = int(input("Days until harvest: "))
+
+    def count_day(day):
+        if day > days:
+            print("Harvest time!")
+        else:
+            print("Day ", day)
+            count_day(day + 1)
+    count_day(1)
